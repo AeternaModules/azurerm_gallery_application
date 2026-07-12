6631ed85484114bfc6c8787814c6c842bb2c2f77
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "gallery_applications" {
-  description = "All gallery_application resources"
-  value       = azurerm_gallery_application.gallery_applications
-}
 output "gallery_applications_description" {
-  description = "List of description values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.description]
+  description = "Map of description values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.description }
 }
 output "gallery_applications_end_of_life_date" {
-  description = "List of end_of_life_date values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.end_of_life_date]
+  description = "Map of end_of_life_date values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.end_of_life_date }
 }
 output "gallery_applications_eula" {
-  description = "List of eula values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.eula]
+  description = "Map of eula values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.eula }
 }
 output "gallery_applications_gallery_id" {
-  description = "List of gallery_id values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.gallery_id]
+  description = "Map of gallery_id values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.gallery_id }
 }
 output "gallery_applications_location" {
-  description = "List of location values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.location]
+  description = "Map of location values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.location }
 }
 output "gallery_applications_name" {
-  description = "List of name values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.name]
+  description = "Map of name values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.name }
 }
 output "gallery_applications_privacy_statement_uri" {
-  description = "List of privacy_statement_uri values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.privacy_statement_uri]
+  description = "Map of privacy_statement_uri values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.privacy_statement_uri }
 }
 output "gallery_applications_release_note_uri" {
-  description = "List of release_note_uri values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.release_note_uri]
+  description = "Map of release_note_uri values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.release_note_uri }
 }
 output "gallery_applications_supported_os_type" {
-  description = "List of supported_os_type values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.supported_os_type]
+  description = "Map of supported_os_type values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.supported_os_type }
 }
 output "gallery_applications_tags" {
-  description = "List of tags values across all gallery_applications"
-  value       = [for k, v in azurerm_gallery_application.gallery_applications : v.tags]
+  description = "Map of tags values across all gallery_applications, keyed the same as var.gallery_applications"
+  value       = { for k, v in azurerm_gallery_application.gallery_applications : k => v.tags }
 }
 
